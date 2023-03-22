a6e6ff90eba8ccc3e3cda0d6c2fefc70c9674f7b
NaraJimenez <njimenezm2122@politecnics.barcelona>
2023-03-22 10:02:47 +0100

    Atascada

--- main.js
+++ main.js
@@ -6,12 +6,52 @@ const app = Vue.createApp({
     data(){
         return{
             //Nos devolvera nuestra data
+            cart:0,
+            priceTotal:0,
             product: 'Socks',
             image: './assets/images/socks_blue.jpg',
-            inventory: 100,
-            //Reto 
-            onSale: true
+            inStock: true,
+            details: ['50% cotton', '30% wool', '20% polyester'],
+            variants: [
+                //RETO CLASE
+              { id: 2234, color: 'green', image: './assets/images/socks_green.jpg', price: 10 },
+              { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg', price: 15 },
+            ]
 
         }
+    },
+
+    //Métodos
+    methods: {
+        addToCart(variantColor){
+            this.cart += 1
+            
+            this.color = variantColor
+
+            if (variantColor = 'green') {
+                variantPrice = 10
+            } else {
+                variantPrice = 15
+            }
+            
+            this.priceTotal += variantPrice
+        },
+        updateImage(variantImage){
+            this.image = variantImage
+        },
+        //RETO CURSO
+        removeItem(variantColor){
+            this.cart -= 1
+            this.color = variantColor
+
+            if (variantColor = 'green') {
+                variantPrice = 10
+            } else {
+                variantPrice = 15
+            }
+            
+            this.priceTotal -= variantPrice
+        }
+        
     }
 })
